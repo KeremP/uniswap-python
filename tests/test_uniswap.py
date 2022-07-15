8a6b0a131ece6209a65cce386ec03ef2417a864b
KeremP <kerpro26@gmail.com>
2022-07-14 23:23:41 -0400

    more asserts

--- tests/test_uniswap.py
+++ tests/test_uniswap.py
@@ -251,9 +251,10 @@ class TestUniswap(object):
         # Ensuring client has sufficient balance of both tokens
         eth_to_dai = client.make_trade(tokens['ETH'], tokens[token0], qty, client.address)
         eth_to_dai_tx = client.w3.eth.wait_for_transaction_receipt(eth_to_dai, timeout=RECEIPT_TIMEOUT)
-
+        assert eth_to_dai_tx["status"]
         dai_to_usdc = client.make_trade(tokens[token0], tokens[token1], amount1, client.address)
         dai_to_usdc_tx = client.w3.eth.wait_for_transaction_receipt(dai_to_usdc, timeout=RECEIPT_TIMEOUT)
+        assert dai_to_usdc_tx["status"]
 
         balance_0 = client.get_token_balance(tokens[token0])
         balance_1 = client.get_token_balance(tokens[token1])
